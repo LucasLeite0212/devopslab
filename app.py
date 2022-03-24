5c27ec7db41e0b6baa6bf2a7e0dc4ed67601b383
Lucas Leite <40664565+LucasLeite0212@users.noreply.github.com>
2022-03-23 23:23:22 -0300

    configuração docker

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+import os 
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
-csrf.init_app(app)
+
 
 @app.route("/")
 def pagina_inicial():
     return "Hello World - Lucas Leite"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
